91e4ba24d2e76ae9cbdfead13b8861f7b12a4a6a
yassineazrak <yassineazrak2019@gmail.com>
2021-06-30 21:20:09 +0100

    logic algorithm done

--- abstract_data.py
+++ abstract_data.py
@@ -8,20 +8,43 @@ class parser(object):
     def validation(self):
         pass
 
-    def get_goal(self):
-        pass
 
-
-class solver(object):
+class Solver(object):
     """class define data struct Puzzle and method need """
-    def __init__(self):
-        pass
 
-    def F(self):
-        pass
+    def __init__(self, puzzleStart, puzzleGoal):
+        self.puzzleStart = Node(puzzleStart, 0, 0)
+        self.puzzleGoal = puzzleGoal
 
-    def H(self):
-        pass
+### I am working with ordered list but priority queue must be implemented to get less element heapq
+    def puzzleSolving(self):
+        Heap = []
+        Heap.append(puzzleStart)
+        while True:
+            puzzleCurrent = Heap[0]
+            del Heap[0]
+            for puzzle in puzzleCurrent.generate_child():
+                item = Node(puzzle, self.F(puzzle, puzzleCurrent.depth))
+                if item.cost == 0:
+                    break
+                Heap.append(item)
+            Heap = sorted(Heap, key=lambda x: x.cost)
+
+    def F(self, puzzle, depth):
+        h = self.H(puzzle)
+        cost = h + depth + 1
+        return cost
+
+    def H(self, puzzle):
+        # gol = self.puzzleGoal
+        gol = [[1, 2, 3], [4, -1, 5], [6, 7, 8]]
+        cost = 0
+        for i in range(len(puzzle)):
+            for j in range(len(puzzle)):
+                if puzzle[i][j] != -1 and puzzle[i][j] == gol[i][j]:
+                    x, y = Node.find(puzzle[i][j])
+                    cost += abs(x - i) + abs(y - j)
+        return cost
 
 
 class Node(object):
@@ -34,11 +57,10 @@ class Node(object):
     def generate_child(self):
         list_childern = []
         stps = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        x, y = self.find(-1)
+        x, y = Node.find(self.graph, -1)
         for stp in stps:
             i, j = stp
             array = self.shuffle(x, y, x + i, y + i)
-            cost = self.get_goal(array)
             depth = self.depth + 1
             list_childern.append(Node(array, depth, cost))
         return list_childern
@@ -51,20 +73,11 @@ class Node(object):
             return puzzle
         return None
 
-    def get_goal(self, puzzle):
-        gol = [[1, 2, 3], [4, -1, 5], [6, 7, 8]]
-        cost = 0
-        for i in range(self.dims):
-            for j in range(self.dims):
-                if puzzle[i][j] != -1:
-                    x, y = self.find(puzzle[i][j])
-                    cost += abs(x - i) + abs(y - j)
-        return cost
-
-    def find(self, elem):
-        for i in range(self.dims):
-            for j in range(self.dims):
-                if self.graph[i][j] == elem:
+    @classmethod
+    def find(cls, puzzle, elem):
+        for i in range(len(puzzle)):
+            for j in range(len(puzzle)):
+                if puzzle[i][j] == elem:
                     return (i, j)
         return None
 
---
+++ main.py
@@ -0,0 +1,11 @@
+from abstract_data import Node ,Solver
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
